f12e5c9f3c474ea2265f0581dd0e76d884bc5637
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-07-13 10:25:44 -0700

    Reformatted the code so its easier to read and displayed keys and values

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,46 @@
-var foods = {apples: 23, 
-  grapes: 507, 
-  eggs: 48}
-
-var person = { height: "6 ft", weight: "160 lbs" }
-
-var post = { imageSrc: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+// I restructed the object so it is easier to read
+var foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
 }
+
+//Displaying the keys and values of 'foods'
+console.log(Object.keys(foods));
+console.log(Object.values(foods));
+
+// I restructure the object so it is easier to read
+var person = {
+  height: "6 ft",
+  weight: "160 lbs"
+}
+
+//Displaying the keys and values of 'person'
+console.log(Object.keys(person))
+console.log(Object.values(person))
+
+//Restructured the object so it is easier to read. Removed
+//key comments since it was empty
+var post = {
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties",
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+}
+
+//Displaying the keys and values of 'post'
+console.log(Object.keys(post));
+console.log(Object.values(post));
+
+//Restructured the object so it is easier to read. Removed
+//key comments since it was empty
+var post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+}
+
+//Displaying the keys and values of "post2"
+console.log(Object.keys(post2))
+console.log(Object.values(post2))
